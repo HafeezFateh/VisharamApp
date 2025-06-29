d54a12dde3b52b47e8d425a88fcc6a70646732ad
Hafeezur Rahman <hafeezofficial08@gmail.com>
2025-06-29 15:33:53 +0530

    Shees

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,15 @@
 const showNav = document.getElementById("responsive-header");
+const color = document.getElementById("colorba");
+function handleColor(){
+  if (color.classList.contains("bg-gradient-to-br")) {
+    color.classList.remove("bg-gradient-to-br");
+    color.classList.add("bg-gray-950");
+  } else {
+    color.classList.remove("bg-gray-950");
+    color.classList.add("bg-gradient-to-br");
+  }
+}
+
 
 function handleClick(){
     showNav.classList.toggle("hidden");
